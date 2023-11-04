45b6d5f6f5fa9d95bf9a1504df99ce2e6a317aa7
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2023-11-04 02:16:13 -0300

    fix: apps and verbose_name

--- apps/common/apps.py
+++ apps/common/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
 
 
 class CommonConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "common"
+    name = "apps.common"
+    verbose_name = _("Common")
