0d1cde89ae79749e48ecc99345bb3382c35d7ca6
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2023-11-11 14:01:39 -0300

    feat: added email notification liked comments

--- apps/likes/emails.py
+++ apps/likes/emails.py
@@ -8,3 +8,16 @@ def send_video_like_notification(sender_user, recipient_user):
     recipient_list = [recipient_user.email]
     
     send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+
+def send_comment_like_notification(sender_user, recipient_user, video):
+
+    video_owner = f"{video.user.first_name}'s video"
+    if (video.user == recipient_user):
+        video_owner = "your video"
+
+    subject = "A user liked your comment"
+    message = f"Hi there, {recipient_user.first_name}!!, the user {sender_user.first_name} {sender_user.last_name} liked your comment on {video_owner}"
+    from_email = settings.DEFAULT_NO_REPLY_EMAIL
+    recipient_list = [recipient_user.email]
+    
+    send_mail(subject, message, from_email, recipient_list, fail_silently=True)
