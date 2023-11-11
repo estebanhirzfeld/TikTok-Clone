6243c35c896416db5d392956df83445f99b25b47
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2023-11-11 13:58:04 -0300

    fix: third person message on email notification

--- apps/comments/emails.py
+++ apps/comments/emails.py
@@ -10,8 +10,13 @@ def send_video_comment_notification(sender_user, recipient_user):
     send_mail(subject, message, from_email, recipient_list, fail_silently=True)
 
 def send_comment_reply_notification(sender_user, recipient_user, video):
+
+    video_owner = f"{video.user.first_name}'s video"
+    if (video.user == recipient_user):
+        video_owner = "your video"
+
     subject = "A user replied your comment"
-    message = f"Hi there, {recipient_user.first_name}!!, the user {sender_user.first_name} {sender_user.last_name} replied your comment on {video.user.first_name}'s video"
+    message = f"Hi there, {recipient_user.first_name}!!, the user {sender_user.first_name} {sender_user.last_name} replied your comment on {video_owner}"
     from_email = settings.DEFAULT_NO_REPLY_EMAIL
     recipient_list = [recipient_user.email]
     
