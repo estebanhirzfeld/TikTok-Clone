166024e6a2a1453b27136f6a2ae558b214f7967a
Esteban Hirzfeld <estebanhirzfeld@gmail.com>
2023-12-11 22:10:15 -0300

    feat: frontend ports allowed by cors in local settings

--- core/settings/local.py
+++ core/settings/local.py
@@ -1,6 +1,5 @@
-from .base import * # noqa
+from .base import *  # noqa
 from .base import env
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -12,9 +11,17 @@ SECRET_KEY = "django-insecure-wj@@x+fhk^_%0)k5nkj$777-2+9wl)1ctw5d1r$w*ype!q^3=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG", True)
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
 
-#TODO: Change "Core"
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000",]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000",]
+
+CORS_ALLOW_CREDENTIALS = True
+
+# TODO: Change "Core"
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
 EMAIL_PORT = env("EMAIL_PORT")
@@ -22,3 +29,6 @@ DEFAULT_FROM_EMAIL = "support@tiktok-clone.site"
 DEFAULT_NO_REPLY_EMAIL = "no-reply@tiktok-clone.site"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "TikTok Clone"
+
+
+
